80c9708ed6796bb7c47e59d5e7ec9c2e5a9911db
stefka04 <stefka1804@gmail.com>
2023-01-20 23:20:03 +0200

    Update

--- menu.h
+++ menu.h
@@ -1,8 +1,21 @@
+/**
+*
+* Solution to course project # 4
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2022/2023
+*
+* @author Stefka Lyaskalieva
+* @idnumber 4MI0600158
+* @compiler VC
+*
+* < menu functions>
+*
+*/
 #pragma once
-#include <iostream>;
+#include <iostream>
 using namespace std;
 void printTheOpeningMenu();
 void start();
 void leaderboard();
-//void quit();
-void checkMenuCommand();
+bool checkMenuCommand();
